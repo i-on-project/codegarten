eff946a3706a128df91feb9b3d18f051af86d301
TiagoMMDavid <tiagommdavid2000@gmail.com>
2021-04-25 10:35:40 +0100

    Added gradle tasks for server application

--- code/jvm/build.gradle.kts
+++ code/jvm/build.gradle.kts
@@ -11,6 +11,8 @@ group = "org.ionproject"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
+val DOCKER_PROJECT_NAME = "CodeGarten"
+
 repositories {
 	mavenCentral()
 }
@@ -40,21 +42,40 @@ tasks.withType<Test> {
 	useJUnitPlatform()
 }
 
+task<Exec>("dbUp") {
+	commandLine("docker-compose", "-p", DOCKER_PROJECT_NAME, "up", "-d", "codegarten-db")
+}
+
+task<Exec>("dbWait") {
+	commandLine("docker", "exec", "codegarten-db", "/app/bin/wait-for-postgres.sh", "localhost")
+	dependsOn("dbUp")
+}
+
+task<Exec>("dbStop") {
+	commandLine("docker", "stop", "codegarten-db")
+}
+
 task<Exec>("dbTestsUp") {
-	commandLine("docker-compose", "up", "-d", "codegarten-db")
+	commandLine("docker-compose", "-p", DOCKER_PROJECT_NAME, "up", "-d", "codegarten-db-tests")
 }
 
 task<Exec>("dbTestsWait") {
-	commandLine("docker", "exec", "codegarten-db", "/app/bin/wait-for-postgres.sh", "localhost")
+	commandLine("docker", "exec", "codegarten-db-tests", "/app/bin/wait-for-postgres.sh", "localhost")
 	dependsOn("dbTestsUp")
 }
 
 task<Exec>("dbTestsDown") {
-	commandLine("docker-compose", "down")
+	commandLine("docker-compose", "-p", DOCKER_PROJECT_NAME, "rm", "-fsv", "codegarten-db-tests")
 }
 
 tasks {
 	named<Test>("test") {
 		dependsOn("dbTestsWait")
+		finalizedBy("dbTestsDown")
+	}
+	
+	named("bootRun") {
+		dependsOn("dbWait")
+		finalizedBy("dbStop")
 	}
 }
