c262c2ae65c647d8bd6f3f88d59f57182967c8f1
PTKickass <jp_moura99@outlook.com>
2021-05-16 01:18:31 +0100

    GH-30: Layout improvements

--- code/js/lib/routes/home-routes.ts
+++ code/js/lib/routes/home-routes.ts
@@ -7,6 +7,7 @@ const router = expressRouter()
 router.get('/', handlerHome)
 
 function handlerHome(req: Request, res: Response, next: NextFunction) {
+    if (req.user) return res.redirect('/orgs')
     res.render('homepage')
 }
 
